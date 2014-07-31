76c5be7eb38831fc0733b987066a62f8d04d17fe
Benoit Pierre <benoit.pierre@gmail.com>
2014-07-31 18:49:57 +0200

    mcomix: cleanup i18n

--- mcomix/i18n.py
+++ mcomix/i18n.py
@@ -3,15 +3,12 @@
 import sys
 import os
 import locale
-import gettext
-import pkg_resources
 
 try:
     import chardet
 except ImportError:
     chardet = None
 
-from mcomix import preferences
 from mcomix import portability
 from mcomix import constants
 
@@ -57,46 +54,5 @@ def to_utf8(string):
         return string.encode('utf-8')
     else:
         return string
-
-def install_gettext():
-    """ Initialize gettext with the correct directory that contains
-    MComix translations. This has to be done before any calls to gettext.gettext
-    have been made to ensure all strings are actually translated. """
-
-    # Add the sources' base directory to PATH to allow development without
-    # explicitly installing the package.
-    sys.path.append(constants.BASE_PATH)
-
-    # Initialize default locale
-    locale.setlocale(locale.LC_ALL, '')
-
-    if preferences.prefs['language'] != 'auto':
-        lang_identifiers = [ preferences.prefs['language'] ]
-    else:
-        # Get the user's current locale
-        code = portability.get_default_locale()
-        lang_identifiers = gettext._expand_lang(code)
-
-    domain = constants.APPNAME.lower()
-    translation = gettext.NullTranslations()
-
-    # Search for .mo files manually, since gettext doesn't support setuptools/pkg_resources.
-    for lang in lang_identifiers:
-        resource = os.path.join(lang, 'LC_MESSAGES', '%s.mo' % domain)
-        if pkg_resources.resource_exists('mcomix.messages', resource):
-            translation = gettext.GNUTranslations(
-                    pkg_resources.resource_stream('mcomix.messages', resource))
-            break
-
-    translation.install(unicode=True)
-
-    global _translation
-    _translation = translation
-
-def get_translation():
-    """ Returns the gettext.Translation instance that has been initialized with
-    install_gettext(). """
-
-    return _translation or gettext.NullTranslations()
 
 # vim: expandtab:sw=4:ts=4
